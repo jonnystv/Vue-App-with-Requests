aeb7ec7730b6224b7ccd62e2fcb7349c416f9400
jonnystv <jonathan@darefoundation.com>
2021-01-15 17:42:07 +0000

    API successfully called into app

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <template lang="html">
+
+  <div id="main-container">
+
+    <h1>Tour de France 2020</h1>
+    <h3>coverage from The Guardian</h3>
+    
+
+
+
+  </div>
   
 </template>
 
 <script>
 export default {
+  name: "app",
 
-}
+  data() {
+    return {
+      TdfCoverage: []
+    }
+  },
+
+mounted() {
+  fetch("https://content.guardianapis.com/search?section=sport&from-date=2020-08-29&to-date=2020-09-20&order-by=oldest&page-size=200&q=tour%20de%20france%202020&api-key=test")
+  .then(res => res.json())
+  .then(TdfCoverageAPI => this.TdfCoverage = TdfCoverageAPI.response.results)
+},
+
+};
 </script>
 
 <style lang="css" scoped>
